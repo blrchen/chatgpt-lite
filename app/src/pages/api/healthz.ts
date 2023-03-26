fcbe556b927b71730bbc3bcf3a51c32092b58f0e
Blair Chen <blrchen@users.noreply.github.com>
2023-03-26 18:22:15 +0000

    Build a backend service with Next.js and extend its functionality to support Azure Open AI (#7)

---
+++ app/src/pages/api/healthz.ts
@@ -0,0 +1,15 @@
+export const config = {
+    runtime: "edge"
+};
+const handler = async (req: any): Promise<Response> => {
+    console.log("Handling request:", req.url);
+    try {
+        const response = new Response("Hello, World!");
+        return response;
+    } catch (error) {
+        console.error(error);
+        return new Response("Internal Server Error", {status: 500});
+    }
+}
+
+export default handler;
---
+++ app/src/typings/global.d.ts
@@ -0,0 +1,6 @@
+declare module '*.less' {
+  const classes: { readonly [key: string]: string }
+  export default classes
+}
+
+declare module 'markdown-it-katex'
