1ae4bc36646058e234f8c59664033f650df69f0d
Steven <Lebonnois.s@odalys-vacances.com>
2024-04-27 22:11:19 +0200

    WIP

--- pages/api/prompts.js
+++ pages/api/prompts.js
@@ -1,18 +1,10 @@
-import { PrismaClient } from '@prisma/client'
 import PromptManager from '@/serveur/PromptManager/PromptManager'
 export default async function handler(req, res) {
-  console.log('in the api  ----------------')
   const promptManager = new PromptManager()
   if (req.method === 'POST') {
-    console.log('in the api  ----------------')
-    console.dir(req.body)
-    console.log('in the api  ----------------')
     // Ajouter un nouveau prompt
     const { name, prompt } = req.body
-    console.log('------------------------------------------------------------')
-    console.log('title', name)
-    console.log('content', prompt)
-    console.log('------------------------------------------------------------')
+
     try {
       promptManager.savePrompt(name, prompt)
 
