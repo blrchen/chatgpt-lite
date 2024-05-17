fae62499d358268b27c54b789c8f27f5c1da99f9
begbed <begbed.work@gmail.com>
2024-05-17 22:58:54 +0700

    fix

--- components/Chat/BotIcon.tsx
+++ components/Chat/BotIcon.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image'
 export const BotIcon = () => (
 	<Image
 		src="/bot-icon.png"
-		alt="TurboChat"
+		alt="Chat"
 		width={32}
 		height={32}
 		className="rounded-full"
